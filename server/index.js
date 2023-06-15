dbbbaded9366294a8ba5b983e537b89cf3bad8f6
Lahiru Supun <lhrsupun@gmail.com>
2023-06-16 00:58:33 +0530

    test

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cors());
 
 app.use(express.json({ limit: '50mb' }));
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 // app.use('/api/*', apiProxy);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/properties', propertyRouter);
