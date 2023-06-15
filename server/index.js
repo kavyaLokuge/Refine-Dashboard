9fc91920835a04fe8db01b9aa209cd698d900329
Lahiru Supun <lhrsupun@gmail.com>
2023-06-16 01:02:28 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cors());
 
 app.use(express.json({ limit: '50mb' }));
 
-app.use(express.static(path.join(__dirname, '..', 'client', 'build', 'index.html')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build'), { index: [ 'index.html'] }));
 // app.use('/api/*', apiProxy);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/properties', propertyRouter);
